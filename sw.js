2c15ad4743bd894ea2743f9907e513c95265a1c3
henicolai <hnicolai@gmail.com>
2026-03-21 14:53:25 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// VaarKaart Service Worker — v3.0
-const CACHE = 'vaarkaart-v3';
+// VaarKaart Service Worker — v4.0
+const CACHE = 'vaarkaart-v4';
 
 // Bestanden die offline beschikbaar moeten zijn
 const OFFLINE_FILES = [
